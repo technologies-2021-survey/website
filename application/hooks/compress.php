0111097920714bfc707ad62b0f2377372cb9ba6b
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-09 01:03:00 +0800

    aw3v

--- application/hooks/compress.php
+++ application/hooks/compress.php
@@ -5,15 +5,14 @@ function compress()
 	$CI =& get_instance();
 	$buffer = $CI->output->get_output();
 	$search = array(
-        //'/ /'
-		//'/\t/',      // replace end of line by a space
-		//'/\>[^\S ]+/s',    // strip whitespaces after tags, except space
-		//'/[^\S ]+\</s'    // strip whitespaces before tags, except space
+		'/\t/',      // replace end of line by a space
+		'/\>[^\S ]+/s',    // strip whitespaces after tags, except space
+		'/[^\S ]+\</s'    // strip whitespaces before tags, except space
 	);
 	$replace = array(
-		//'',
-		//'>',
-		//'<'
+		'',
+		'>',
+		'<'
 	);
 	$buffer = preg_replace($search, $replace, $buffer);
 	$CI->output->set_output($buffer);
