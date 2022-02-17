b37c52e38618bc6d83f986c0ce1b50bcd7c653fd
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-17 18:15:26 +0800

    asd

--- bookacleaner/app/Views/Home/index.php
+++ bookacleaner/app/Views/Home/index.php
@@ -11,11 +11,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		
-		<link rel="icon" type="image/png" href="<?php echo base_url() . "assets/img/whealth.png";?>">
+		<link rel="icon" type="image/png" href="<?php echo base_url() . "public/assets/img/whealth.png";?>">
 		
-		<link rel="stylesheet" href="<?=base_url('assets/css/bootstrap.min.css');?>" media="screen">
-        <script type="text/javascript" src="<?=base_url('assets/js/jquery-1.10.2.min.js');?>"></script>
-		<script type="text/javascript" src="<?=base_url('assets/js/bootstrap.min.js');?>"></script>
+		<link rel="stylesheet" href="<?=base_url('public/assets/css/bootstrap.min.css');?>" media="screen">
+        <script type="text/javascript" src="<?=base_url('public/assets/js/jquery-1.10.2.min.js');?>"></script>
+		<script type="text/javascript" src="<?=base_url('public/assets/js/bootstrap.min.js');?>"></script>
 		<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         
