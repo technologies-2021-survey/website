2b603df9a6b898b510ffeddf8b4f17f5ca728f6b
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-17 18:12:34 +0800

    Update index.php

--- bookacleaner/app/Views/Home/index.php
+++ bookacleaner/app/Views/Home/index.php
@@ -3,8 +3,29 @@
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>{elapsed_time} - BookACleaner</title>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>{elapsed_time} - BookACleaner</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		
+		<link rel="icon" type="image/png" href="<?php echo base_url() . "assets/img/whealth.png";?>">
+		
+		<link rel="stylesheet" href="<?=base_url('assets/css/bootstrap.min.css');?>" media="screen">
+		<link rel="stylesheet" href="<?=base_url('assets/css/style.css');?>" media="screen">
+		<link rel="stylesheet" href="<?=base_url('assets/css/all.min.css');?>" media="screen">
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+			<script src="../bower_components/html5shiv/dist/html5shiv.js"></script>
+			<script src="../bower_components/respond/dest/respond.min.js"></script>
+		<![endif]-->
+        <script type="text/javascript" src="<?=base_url('assets/js/jquery-1.10.2.min.js');?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/bootstrap.min.js');?>"></script>
+		<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        
     </head>
     <body>
         <?php echo view('Home/navigation.php'); ?>
