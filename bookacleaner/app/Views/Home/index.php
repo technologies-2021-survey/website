d2d63dfa915a79def82d80b68dd7da72b1805899
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-17 18:13:57 +0800

    Update index.php

--- bookacleaner/app/Views/Home/index.php
+++ bookacleaner/app/Views/Home/index.php
@@ -14,13 +14,6 @@
 		<link rel="icon" type="image/png" href="<?php echo base_url() . "assets/img/whealth.png";?>">
 		
 		<link rel="stylesheet" href="<?=base_url('assets/css/bootstrap.min.css');?>" media="screen">
-		<link rel="stylesheet" href="<?=base_url('assets/css/style.css');?>" media="screen">
-		<link rel="stylesheet" href="<?=base_url('assets/css/all.min.css');?>" media="screen">
-		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!--[if lt IE 9]>
-			<script src="../bower_components/html5shiv/dist/html5shiv.js"></script>
-			<script src="../bower_components/respond/dest/respond.min.js"></script>
-		<![endif]-->
         <script type="text/javascript" src="<?=base_url('assets/js/jquery-1.10.2.min.js');?>"></script>
 		<script type="text/javascript" src="<?=base_url('assets/js/bootstrap.min.js');?>"></script>
 		<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
