812925a234be63ff98838865f6b8964f1a0f9247
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-19 01:43:11 +0800

    Update index.php

--- bookacleaner/app/Views/Home/index.php
+++ bookacleaner/app/Views/Home/index.php
@@ -18,11 +18,14 @@
 		<link rel="stylesheet" href="<?=base_url('public/assets/css/style.css');?>" media="screen" />
 
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		
         <script type="text/javascript" src="<?=base_url('public/assets/js/jquery-1.10.2.min.js');?>"></script>
 		<script type="text/javascript" src="<?=base_url('public/assets/js/bootstrap.min.js');?>"></script>
-		<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<script type="text/javascript" src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+		<script type="text/javascript" src="https://unpkg.com/scrollreveal"></script>
+
+		
         
     </head>
     <body>
