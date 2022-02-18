15c58c164af23b364b94bba0288afbfa9161a778
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-19 00:15:32 +0800

    Logo

--- bookacleaner/app/Views/Home/content.php
+++ bookacleaner/app/Views/Home/content.php
@@ -1,3 +1,11 @@
 <section class="header">
-    <div class="header2"></div>
+    <div class="header2">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="logo"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
