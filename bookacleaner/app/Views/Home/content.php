2ef0bfaaa4329ae1e9234a306bae1aafa1ac1431
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-19 01:06:53 +0800

    Update content.php

--- bookacleaner/app/Views/Home/content.php
+++ bookacleaner/app/Views/Home/content.php
@@ -20,22 +20,69 @@
         <h4>Services</h4>
         <div class="row">
             <div class="col-sm-4 col-xs-12">
-                Service
+                <div class="services-box">
+                    <div class="services-title">
+                        Deep Cleaning
+                    </div>
+                    <div class="services-desc">
+                        A total cleaning for your home or office. Full sweep, wipe down, sanitizing and disinfecting. A team of 2 professional cleaner at 5 hours per session (depending on the size of your home or office).
+                    </div>
+                </div>
             </div>
+
             <div class="col-sm-4 col-xs-12">
-                Service
+                <div class="services-box">
+                    <div class="services-title">
+                        Disinfection Services
+                    </div>
+                    <div class="services-desc">
+                        Different cleaning and sanitation services to complement the deep cleaning for your home or office. Tell us cwhich cleaning and sanitation service is best for your needs.
+                    </div>
+                </div>
             </div>
+
             <div class="col-sm-4 col-xs-12">
-                Service
+                <div class="services-box">
+                    <div class="services-title">
+                        Move In & Move Out Cleaning
+                    </div>
+                    <div class="services-desc">
+                        Moving in and out of your home or office can be a very daunting and stressful task. We can help lighten your work load.
+                    </div>
+                </div>
             </div>
+
             <div class="col-sm-4 col-xs-12">
-                Service
+                <div class="services-box">
+                    <div class="services-title">
+                        Upholstery Cleaning
+                    </div>
+                    <div class="services-desc">
+                        It makes use of vacuum cleaning system with ecological pure water filter technology. We remove deep-seated dirt and allergens.
+                    </div>
+                </div>
             </div>
+
             <div class="col-sm-4 col-xs-12">
-                Service
+                <div class="services-box">
+                    <div class="services-title">
+                        Steam Cleaning
+                    </div>
+                    <div class="services-desc">
+                        It makes use of Dry Steam cleaning machine. Picks up dust, removes stains, kills germs and bacteria, disinfects, sanitize all types of hard surfaces without the use of chemicals.
+                    </div>
+                </div>
             </div>
+
             <div class="col-sm-4 col-xs-12">
-                Service
+                <div class="services-box">
+                    <div class="services-title">
+                        Aircon Cleaning
+                    </div>
+                    <div class="services-desc">
+                        Thoroughly clean all types and models of air conditioning systems.
+                    </div>
+                </div>
             </div>
         </div>
     </div>
