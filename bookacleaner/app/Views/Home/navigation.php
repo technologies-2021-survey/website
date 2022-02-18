8b32064aeffb736863c33824a1127c339f797e02
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-18 23:53:07 +0800

    Logo

--- bookacleaner/app/Views/Home/navigation.php
+++ bookacleaner/app/Views/Home/navigation.php
@@ -7,7 +7,9 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">BookACleaner</a>
+            <a class="navbar-brand" href="#">
+                <img src="<?=base_url('public/assets/img/logo.png');?>">
+            </a>
         </div>
 
         <div class="collapse navbar-collapse" id="bookacleaner">
