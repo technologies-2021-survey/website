888641c0e832d66ebc33f886e9a1918b0ac1a11e
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-20 01:02:22 +0800

    Update content.php

--- bookacleaner/app/Views/Home/content.php
+++ bookacleaner/app/Views/Home/content.php
@@ -182,12 +182,12 @@
 </section>
 
 <!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="bookNow" tabindex="-1" role="dialog" aria-labelledby="bookNowLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                <h4 class="modal-title" id="bookNowLabel">Modal title</h4>
             </div>
             <div class="modal-body">
                 ...
