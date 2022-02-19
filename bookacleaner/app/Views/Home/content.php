4229f5249b36ef16bd9b6ae2a8ec3b7a80cdbc56
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-19 22:48:19 +0800

    Update content.php

--- bookacleaner/app/Views/Home/content.php
+++ bookacleaner/app/Views/Home/content.php
@@ -130,7 +130,7 @@
 <section class="book" id="book">
     <div class="container">
         <h3>Book a service or get a quote today</h3>
-        <div class="row">
+        <div class="row" style="background:#000;">
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
                 
                 <img src="<?=base_url('../public/assets/img/book.png');?>" style="width: 100%; margin: 0 auto; display: block; margin-left: -15px; }">
