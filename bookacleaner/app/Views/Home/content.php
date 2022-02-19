f260789f0273b44034ee3eb398ae6e7456816c90
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-20 01:05:29 +0800

    Update content.php

--- bookacleaner/app/Views/Home/content.php
+++ bookacleaner/app/Views/Home/content.php
@@ -187,7 +187,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="bookNowLabel">Modal title</h4>
+                <h4 class="modal-title" id="bookNowLabel">Book a service with us today!</h4>
             </div>
             <div class="modal-body">
                 ...
