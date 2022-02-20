b84fba000efeaab58175990535d4b551355ad496
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-20 22:49:28 +0800

    Update login.php

--- bookacleaner/app/Views/Admin/login.php
+++ bookacleaner/app/Views/Admin/login.php
@@ -12,9 +12,9 @@
                             </div>
                         </div> 
                     </div>
-                    <div class="rows" style="margin-bottom: 0;">
+                    <div class="rows">
                         <div class="col-lg-12">
-                            <div class="form-group">
+                            <div class="form-group" style="margin-bottom: 0;">
                                 <input type="password" name="password" id="password" class="form-control input-lg" tabindex="2">
                                 <label class="floatingText">Password</label>
                             </div>
