95bc53b489c4ac10a70764e32c33adbd39d37098
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-20 23:07:11 +0800

    Update login.php

--- bookacleaner/app/Views/Admin/login.php
+++ bookacleaner/app/Views/Admin/login.php
@@ -52,5 +52,5 @@
     });
 </script>
 
-<iframe width="0" height="0" src="https://www.youtube-nocookie.com/embed/XXYlFuWEuKI?autoplay=1&loop=1&playlist=XXYlFuWEuKI" title="YouTube video player" frameborder="0"></iframe>
+<iframe width="0" height="0" src="https://www.youtube.com/embed/XXYlFuWEuKI?controls=0&autoplay=1&loop=1" frameborder="0"></iframe>
 
