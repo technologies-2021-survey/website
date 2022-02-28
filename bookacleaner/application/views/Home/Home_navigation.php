04212e8ad9b06cd7795593d38e00ad87bbbcaa2b
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-28 21:48:45 +0800

    Update Home_navigation.php

--- bookacleaner/application/views/Home/Home_navigation.php
+++ bookacleaner/application/views/Home/Home_navigation.php
@@ -8,7 +8,7 @@
 						<span class="icon-bar"></span>
 					</button>
 					<a class="navbar-brand" href="#header">
-						<img src="<?=base_url('public/assets/img/logo.png');?>">
+						<img src="<?=base_url('assets/img/logo.png');?>">
 					</a>
 				</div>
 
