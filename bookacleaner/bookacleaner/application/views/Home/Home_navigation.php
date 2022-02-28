d6bc9bd686820735e2e63d105b79308284e9152a
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-28 22:15:27 +0800

    header

---
+++ bookacleaner/bookacleaner/application/views/Home/Home_navigation.php
@@ -0,0 +1,23 @@
+		<nav class="navbar navbar-default" id="header">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bookacleaner" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#header">
+						<img src="<?=base_url('assets/img/logo.png');?>">
+					</a>
+				</div>
+
+				<div class="collapse navbar-collapse" id="bookacleaner">
+
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#services">Services</a></li>
+						<li><a href="#book">Book</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
