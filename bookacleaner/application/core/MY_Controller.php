54168bcde954ade71563eab4e9965887d1aa0fe1
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-28 22:00:47 +0800

    asd

--- bookacleaner/application/core/MY_Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class MY_Controller extends CI_Controller { 
-	public function home_page($page, $arrayData = []) {
-		$this->load->view('Home/Include/header.php', $arrayData);
-		$this->load->view('Home/'.$page);
-		$this->load->view('Home/Include/footer.php');
-	}
-}
