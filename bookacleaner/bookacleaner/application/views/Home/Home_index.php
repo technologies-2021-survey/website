1d50767509459955c64051311fe77b051982e573
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-01 01:05:38 +0800

    Update Home_index.php

--- bookacleaner/bookacleaner/application/views/Home/Home_index.php
+++ bookacleaner/bookacleaner/application/views/Home/Home_index.php
@@ -192,141 +192,143 @@
 				<div class="modal-content">
 					<h4 style="text-align: center; margin-top: 22px;">Book a service with us today!</h4>
 					<hr class="colorgraph" />
-					<div class="modal-body">
-						<form id="book">
-							<div class="rows">
-								<div class="col-xs-6 col-sm-6 col-md-6">
-									<div class="form-group">
-										<input type="text" name="first_name" id="first_name" class="form-control input-lg" tabindex="1" autocomplete="off" required="">
-										<label class="floatingText">First Name</label>
+					<form id="book">
+						<div class="modal-body">
+							
+								<div class="rows">
+									<div class="col-xs-6 col-sm-6 col-md-6">
+										<div class="form-group">
+											<input type="text" name="first_name" id="first_name" class="form-control input-lg" tabindex="1" autocomplete="off" required="">
+											<label class="floatingText">First Name</label>
+										</div>
+									</div>
+									<div class="col-xs-6 col-sm-6 col-md-6">
+										<div class="form-group">
+											<input type="text" name="last_name" id="last_name" class="form-control input-lg" tabindex="2" autocomplete="off" required="">
+											<label class="floatingText">Last Name</label>
+										</div>
 									</div>
 								</div>
-								<div class="col-xs-6 col-sm-6 col-md-6">
-									<div class="form-group">
-										<input type="text" name="last_name" id="last_name" class="form-control input-lg" tabindex="2" autocomplete="off" required="">
-										<label class="floatingText">Last Name</label>
-									</div>
-								</div>
-							</div>
-							<div class="rows">
-								<div class="col-lg-12">
-									<div class="form-group">
-										<input type="email" name="email" id="email" class="form-control input-lg" tabindex="3" autocomplete="off" required="">
-										<label class="floatingText">E-mail Address</label>
-									</div>
-								</div> 
-							</div>
-							<div class="rows">
-								<div class="col-xs-6 col-sm-6 col-md-6">
-									<div class="form-group">
-										<input type="text" name="mobile_number" id="mobile_number" class="form-control input-lg" pattern="^(09|\+639)\d{9}$" tabindex="4" autocomplete="off" required="">
-										<label class="floatingText">Mobile Number</label>
+								<div class="rows">
+									<div class="col-lg-12">
+										<div class="form-group">
+											<input type="email" name="email" id="email" class="form-control input-lg" tabindex="3" autocomplete="off" required="">
+											<label class="floatingText">E-mail Address</label>
+										</div>
 									</div> 
 								</div>
-								<div class="col-xs-6 col-sm-6 col-md-6">
-									<div class="form-group">
-										<input type="date" name="preferred_date" id="preferred_date" class="form-control input-lg" tabindex="5" autocomplete="off" required="">
-										<label class="floatingText">Preferred Date</label>
+								<div class="rows">
+									<div class="col-xs-6 col-sm-6 col-md-6">
+										<div class="form-group">
+											<input type="text" name="mobile_number" id="mobile_number" class="form-control input-lg" pattern="^(09|\+639)\d{9}$" tabindex="4" autocomplete="off" required="">
+											<label class="floatingText">Mobile Number</label>
+										</div> 
+									</div>
+									<div class="col-xs-6 col-sm-6 col-md-6">
+										<div class="form-group">
+											<input type="date" name="preferred_date" id="preferred_date" class="form-control input-lg" tabindex="5" autocomplete="off" required="">
+											<label class="floatingText">Preferred Date</label>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div class="rows">
-								<div class="col-lg-12">
-									<div class="form-group">
-										<input type="text" name="address" id="address" class="form-control input-lg" tabindex="6" autocomplete="off" required="">
-										<label class="floatingText">Address</label>
+								<div class="rows">
+									<div class="col-lg-12">
+										<div class="form-group">
+											<input type="text" name="address" id="address" class="form-control input-lg" tabindex="6" autocomplete="off" required="">
+											<label class="floatingText">Address</label>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div class="rows">
-								<div class="col-lg-12">
-									<div class="form-group">
-										<select name="cleaning" class="form-control input-lg" tabindex="7" autocomplete="off" required="">
-											<option selected="">Please select</option>
-											<option value="1">Appartment</option>
-											<option value="2">House</option>
-											<option value="3">Condo</option>
-											<option value="4">Office</option>
-											<option value="5">Others (Plesase specify in NOTES)</option>
-										</select>
-										<label class="floatingText">I need cleaning for</label>
+								<div class="rows">
+									<div class="col-lg-12">
+										<div class="form-group">
+											<select name="cleaning" class="form-control input-lg" tabindex="7" autocomplete="off" required="">
+												<option selected="">Please select</option>
+												<option value="1">Appartment</option>
+												<option value="2">House</option>
+												<option value="3">Condo</option>
+												<option value="4">Office</option>
+												<option value="5">Others (Plesase specify in NOTES)</option>
+											</select>
+											<label class="floatingText">I need cleaning for</label>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div class="rows">
-								<div class="col-lg-12">
-									<div class="form-group">
-										<input type="text" name="sqm" id="sqm" class="form-control input-lg" tabindex="8" autocomplete="off" required="">
-										<label class="floatingText">How big is the space that needs cleaning? (sqm.)</label>
+								<div class="rows">
+									<div class="col-lg-12">
+										<div class="form-group">
+											<input type="text" name="sqm" id="sqm" class="form-control input-lg" tabindex="8" autocomplete="off" required="">
+											<label class="floatingText">How big is the space that needs cleaning? (sqm.)</label>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div class="rows">
-								<div class="col-lg-12">
-									<label>Service Required</label>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" name="serviceRequired" value="Deep Cleaning">
-											<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
-											Deep Cleaning
-										</label>
-									</div>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" name="serviceRequired" value="Disinfection Service">
-											<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
-											Disinfection Service
-										</label>
-									</div>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" name="serviceRequired" value="Move In & Move Out Cleaning">
-											<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
-											Move In & Move Out Cleaning
-										</label>
-									</div>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" name="serviceRequired" value="Upholstery Cleaning">
-											<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
-											Upholstery Cleaning
-										</label>
-									</div>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" name="serviceRequired" value="Steam Cleaning">
-											<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
-											Steam Cleaning
-										</label>
-									</div>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" name="serviceRequired" value="Aircon Cleaning">
-											<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
-											Aircon Cleaning
-										</label>
-									</div>
-								</div>
-							</div>
-							<div class="rows">
-								<div class="col-lg-12">
-									<div class="form-group">
-										<textarea type="text" name="comments_or_notes" id="comments_or_notes" class="form-control input-lg" tabindex="8" autocomplete="off" required=""></textarea>
-										<label class="floatingText">Comments/Notes</label>
+								<div class="rows">
+									<div class="col-lg-12">
+										<label>Service Required</label>
+										<div class="checkbox">
+											<label>
+												<input type="checkbox" name="serviceRequired" value="Deep Cleaning">
+												<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
+												Deep Cleaning
+											</label>
+										</div>
+										<div class="checkbox">
+											<label>
+												<input type="checkbox" name="serviceRequired" value="Disinfection Service">
+												<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
+												Disinfection Service
+											</label>
+										</div>
+										<div class="checkbox">
+											<label>
+												<input type="checkbox" name="serviceRequired" value="Move In & Move Out Cleaning">
+												<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
+												Move In & Move Out Cleaning
+											</label>
+										</div>
+										<div class="checkbox">
+											<label>
+												<input type="checkbox" name="serviceRequired" value="Upholstery Cleaning">
+												<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
+												Upholstery Cleaning
+											</label>
+										</div>
+										<div class="checkbox">
+											<label>
+												<input type="checkbox" name="serviceRequired" value="Steam Cleaning">
+												<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
+												Steam Cleaning
+											</label>
+										</div>
+										<div class="checkbox">
+											<label>
+												<input type="checkbox" name="serviceRequired" value="Aircon Cleaning">
+												<span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
+												Aircon Cleaning
+											</label>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div style="clear:both;"></div>
-						</form>
-					</div>
-					<hr class="colorgraph" style="margin-bottom: -1px;" />
-					<div class="modal-footer" style="border: 0;">
-						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button name="submit" class="btn btn-success">
-							<i class="fa fa-paper-plane" aria-hidden="true" style="margin-right: 5px;"></i>
-							Submit Booking
-						</button>
-					</div>
+								<div class="rows">
+									<div class="col-lg-12">
+										<div class="form-group">
+											<textarea type="text" name="comments_or_notes" id="comments_or_notes" class="form-control input-lg" tabindex="8" autocomplete="off" required=""></textarea>
+											<label class="floatingText">Comments/Notes</label>
+										</div>
+									</div>
+								</div>
+								<div style="clear:both;"></div>
+							
+						</div>
+						<hr class="colorgraph" style="margin-bottom: -1px;" />
+						<div class="modal-footer" style="border: 0;">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+							<button name="submit" class="btn btn-success">
+								<i class="fa fa-paper-plane" aria-hidden="true" style="margin-right: 5px;"></i>
+								Submit Booking
+							</button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
