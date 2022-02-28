c48d07d3efbd1d0ab9fc69560668ddb8ab536491
Harvey Arboleda <haaaarvs24@gmail.com>
2022-02-28 23:18:28 +0800

    Update Admin_main.php

--- bookacleaner/bookacleaner/application/views/Admin/Admin_main.php
+++ bookacleaner/bookacleaner/application/views/Admin/Admin_main.php
@@ -1,12 +1,10 @@
-<div class="container">
-    <div class="col-lg-4">
-        <div class="box-container">
-            <div class="box-body">
-                Hello, <?=$username;?>!
-            </div>
+<div class="col-lg-4">
+    <div class="box-container">
+        <div class="box-body">
+            Hello, <?=$username;?>!
         </div>
     </div>
-    <div class="col-lg-8">
+</div>
+<div class="col-lg-8">
 
-    </div>
 </div>
