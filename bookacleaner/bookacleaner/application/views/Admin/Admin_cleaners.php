4f3a5b688373e4a0386138d1684603094fc799ef
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-03 22:40:30 +0800

    Update Admin_cleaners.php

--- bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
+++ bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
@@ -2,9 +2,26 @@
     <div class="box-container">
         <div class="box-body">
             <h3>Cleaner's List</h3>
-            <div class="">
-
+            <div class="cleaners-list">
             </div>
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    var id = 0
+    $(document).ready(function() {
+        function getCleaners(id) {
+            $.ajax({
+				url: "<?php echo base_url(); ?>admin/getCleaners",
+				type: "POST",
+				data: {
+					id: id,					
+				},
+				success: function(data){
+                    console.log(data)
+                }
+            });
+        }
+    });
+</script>
