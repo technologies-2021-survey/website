56457d2056ef327563292b21a4f7d20bf2a188df
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-03 23:10:33 +0800

    Update Admin_cleaners.php

--- bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
+++ bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
@@ -26,13 +26,13 @@
         });
     }
     function addRow(data) {
-        var x = '<div class="cleaners-row row-'+data.id+'" style="opacity: 0;margin-left:-200px;">';
+        var x = '<div class="cleaners-row row-'+data.id+'" style="opacity:margin-left:-200px;">';
             x = x + data.cleaners_name;
             x = x + '<br>';
             x = x + data.cleaners_contact;
         x = x + '</div>';
         $('.cleaners-list').append(x);
-		$('.row-'+data.id).show().animate({ opacity: 1, marginLeft: "0px"}, { duration: 'slow', easing: 'easeOutBack'});
+		$('.row-'+data.id).show().css({ opacity: 1, marginLeft: "0px"}, { duration: 'slow', easing: 'easeOutBack'});
     }
 
     getCleaners(id);
