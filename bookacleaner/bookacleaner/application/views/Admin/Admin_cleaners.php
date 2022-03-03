a811a5293687772a61c43e49baf122f490110688
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-04 03:27:07 +0800

    Update Admin_cleaners.php

--- bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
+++ bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
@@ -1,10 +1,12 @@
 <div class="col-lg-6 col-md-6">
     <div class="box-container">
         <div class="box-body">
-            <h3>Cleaner's List</h3>
-            <div class="pull-right">
-                <button class="btn btn-success">Add Cleaner</button>
-            </div>
+            <h3>
+                Cleaner's List
+                <div class="pull-right">
+                    <button class="btn btn-success">Add Cleaner</button>
+                </div>
+            </h3>
             <div class="cleaners-list" style="clear: both; margin-bottom: 10px;">
             </div>
             <div class="pull-left">
