458fbe6ac2fff31874810479ee857e5dde2fd5c3
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-03 23:37:17 +0800

    Update Admin_cleaners.php

--- bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
+++ bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
@@ -37,7 +37,7 @@
         
         var durations = id * 1000;
         $('.row-'+id).hide().css({ opacity: 0, marginLeft: "200px"});
-        $('.row-'+id).show(durations).animate({ opacity: 1, marginLeft: "0px"}, { duration: slow, easing: 'easeOutBack'});
+        $('.row-'+id).show(durations).animate({ opacity: 1, marginLeft: "0px"}, { duration: 'slow', easing: 'easeOutBack'});
         console.log(durations)
     }
 
