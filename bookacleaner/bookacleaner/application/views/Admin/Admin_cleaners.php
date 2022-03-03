c8e47bdf63caae305b6c4a626253575c2e7e2fac
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-03 23:50:50 +0800

    Update Admin_cleaners.php

--- bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
+++ bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
@@ -27,6 +27,7 @@
             success: function(data){
                 data = JSON.parse(data);
                 if(data.length != "") {
+                    $('.cleaners-list').html("");
                     for(var i = 0; i < data.length * 1000; i++) {
                         addRow(data[i]);
                     }
