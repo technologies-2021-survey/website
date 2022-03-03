d4ab3bc3c0a90a88f5f66265040cede20564c818
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-03 22:44:34 +0800

    Update Admin_cleaners.php

--- bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
+++ bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
@@ -10,21 +10,29 @@
 
 <script type="text/javascript">
     var id = 0
-    $(document).ready(function() {
-        function getCleaners(id) {
-            $.ajax({
-				url: "<?php echo base_url(); ?>admin/getCleaners",
-				type: "POST",
-				data: {
-					id: id,					
-				},
-				success: function(data){
-                    console.log(data)
+    function getCleaners(id) {
+        $.ajax({
+            url: "<?php echo base_url(); ?>admin/getCleaners",
+            type: "POST",
+            data: {
+                id: id,					
+            },
+            success: function(data){
+                for(var i = 0; i < data.length; i++) {
+                    addRow(data[i]);
                 }
-            });
-        }
+            }
+        });
+    }
+    function addRow(data) {
+        var x = '<div class="cleaners-row row-'+data.id+'" style="display: none;">';
+            x = x + data.cleaners_name;
+            x = x + '<br>';
+            x = x + data.cleaners_contact;
+        x = x + '</div>';
+        $('.cleaners-list').append(x);
+        $('.row-'+data.id).fadeOut();
+    }
 
-
-        getCleaners(0);
-    });
+    getCleaners(0);
 </script>
