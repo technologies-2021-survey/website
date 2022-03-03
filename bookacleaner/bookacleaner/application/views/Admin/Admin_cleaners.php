e89e0a7d1542dfeddb33fddd57195abbe6ba0005
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-03 22:38:43 +0800

    Update Admin_cleaners.php

--- bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
+++ bookacleaner/bookacleaner/application/views/Admin/Admin_cleaners.php
@@ -1,8 +1,10 @@
 <div class="col-lg-6 col-md-6">
     <div class="box-container">
         <div class="box-body">
-            <h3>Calendar</h3>
-            <div id="calendar"></div>
+            <h3>Cleaner's List</h3>
+            <div class="">
+
+            </div>
         </div>
     </div>
 </div>
