4f0c59cdacdd8a6c1a8aabf2dd85ca2247edfd62
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-06 17:17:19 +0800

    Update Admin_table.php

--- food/food/application/views/Admin/Admin_table.php
+++ food/food/application/views/Admin/Admin_table.php
@@ -150,6 +150,10 @@
                 x = x + '<label class="label label-success">'+data.status+'</label>';
             }
             x = x + '<label class="label label-primary" style="margin-left: 5px;"><i class="fa fa-dashboard" style="margin-right: 5px;"></i>'+moment.unix(data.time).utc().fromNow()+'</label>';
+            x = x + '<div class="pull-right">';
+                x = x + '<button class="btn btn-success">View Order</button>';
+            x = x + '</div>';
+            x = x + '<div style="clear: both;"></div>';
         x = x + '</div>';
         $('.dine-in-list').append(x);
         
