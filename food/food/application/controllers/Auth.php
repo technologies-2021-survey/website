76e89b7d765889444e5ae2756a353981b30ac6bb
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-07 00:07:54 +0800

    a

---
+++ food/food/application/controllers/Auth.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+date_default_timezone_set("Asia/Manila");
+
+class Auth extends CI_Controller {
+    public function index() {
+        json_output(
+            400,
+            array(
+                "status" => 400,
+                "message" => 'Bad Request'
+            )
+        );
+    }
+    public function login() {
+        $this->load->model('auth_model', null, true);
+
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method != "POST") {
+            json_output(
+                400,
+                array(
+                    "status" => 400,
+                    "message" => 'Bad Request'
+                )
+            );
+        } else {
+            $checkAuth = $this->auth_model->check_auth();
+            if($checkAuth == true) {
+                $params = $_REQUEST;
+                $username = $params['username'];
+                $password = $params['password'];
+
+                $response = $this->auth_model->login($username, $password);
+                echo json_encode($response);
+                json_output($response['status'], $response);
+            }
+        }
+    }
+}
+?>
---
+++ food/food/application/models/Auth_model.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth_model extends CI_Model {
+    var $client_service = "cafelidia-client";
+    var $auth_key = "cafelidiaApi!@";
+
+    public function check_auth() {
+        $client_service = $this->input->get_request_header("Client-Service", TRUE);
+        $auth_key = $this->input->get_request_header("Auth-Key", TRUE);
+
+        if($client_service == $this->client_service && $auth_key == $this->auth_key) {
+            return true;
+        } else {
+            return json_output(
+                401,
+                array(
+                    'status' => 401,
+                    'message' => 'Unauthorized.'
+                )
+            );
+        }
+    }
+    public function login($username, $password) {
+        $q = $this->db->select('id, username, password')->from("customer_accounts")->where(
+            array(
+                'username' => $username, 
+                'password' => $password)
+            )->get()->row();
+        if($q == "") {
+            return array(
+                'status' => 204,
+                'message' => "Invalid username/password."
+            );
+        } else {
+            return array(
+                'status' => 200,
+                'message' => "Successfully login!",
+                'username' => $username,
+                'customer_id' => $q->id
+            );
+        }
+    }
+}
+?>
