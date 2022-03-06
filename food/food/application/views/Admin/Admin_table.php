d71e4dbe5244544d5f9410028b921751ff15eeb8
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-06 17:00:50 +0800

    Update Admin_table.php

--- food/food/application/views/Admin/Admin_table.php
+++ food/food/application/views/Admin/Admin_table.php
@@ -24,8 +24,29 @@
     </div>
 </div>
 
+<div class="col-lg-6 col-md-6">
+    <div class="box-container"> 
+        <div class="box-body">
+            <h3>
+                Dine In(s)
+            </h3>
+            
+            <div class="dine-in-list" style="margin-bottom: 10px;">
+            </div>
+            <div class="pull-left">
+                <button class="btn btn-primary" id="prev2"><i class="fa fa-caret-left" aria-hidden="true"></i>&nbsp;Prev</button>
+            </div>
+            <div class="pull-right">
+                <button class="btn btn-primary" id="next2">Next&nbsp;<i class="fa fa-caret-right" aria-hidden="true"></i></button>
+            </div>
+            <div style="clear: both;"></div>
+        </div>
+    </div>
+</div>
+
 <script type="text/javascript">
     var id = 1;
+    var id2 = 1;
     function checkTables(ids, type = "") {
         $.ajax({
             url: "<?php echo base_url(); ?>admin/getTables/"+ids,
@@ -63,6 +84,45 @@
             }
         });
     }
+
+    function checkDineIn(ids, type = "") {
+        $.ajax({
+            url: "<?php echo base_url(); ?>admin/getDineIn/"+ids,
+            type: "GET",
+            success: function(data){
+                data = JSON.parse(data);
+                if(data.length != "") {
+                    notif.play();
+                    if(type == "add") {
+                        id2++;
+                        getDineIn(id2);
+                    } else if(type == "minus") {
+                        id2--;
+                        getDineIn(id2);
+                    }
+                } else {
+                    errorRow();
+                }
+                
+            }
+        });
+    }
+    function getDineIn(ids) {
+        $.ajax({
+            url: "<?php echo base_url(); ?>admin/getDineIn/"+id2,
+            type: "GET",
+            success: function(data){
+                data = JSON.parse(data);
+                $('.table-list').html("");
+                if(data.length != "") {
+                    for(var i = 0; i < data.length; i++) {
+                        addRow(i, data[i]);
+                    }
+                }
+            }
+        });
+    }
+
     function addRow(i, data) {
         var x = '<div class="accounts-row row-'+data.id+'">';
             x = x + '<span>'+data.table_name+'</span>';
@@ -88,6 +148,7 @@
     }
 
     getTables(id);
+    getDineIn(id2);
 
     $(document).ready(function() {
         $('#prev').click(function() {
@@ -101,6 +162,19 @@
         $('#next').click(function() {
             var s = id + 1;
             checkTables(s, 'add');
+        });
+
+        $('#prev2').click(function() {
+            if(id2 > 1) {
+                var s = id2 - 1;
+                checkDineIn(s2, 'minus');
+            } else {
+                errorRow();
+            }
+        });
+        $('#next2').click(function() {
+            var s = id2 + 1;
+            checkDineIn(s, 'add');
         });
     });
 </script>
