635b2a666cc5a19e21cc0f874c65102ea2d77af4
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-06 16:48:59 +0800

    Update Admin_table.php

--- food/food/application/views/Admin/Admin_table.php
+++ food/food/application/views/Admin/Admin_table.php
@@ -1,13 +1,16 @@
 <div class="col-lg-6 col-md-6">
     <div class="box-container"> 
         <div class="box-body">
-            <h3>Table(s)</h3>
-            <div class="pull-right">
-                <button class="btn btn-success">
-                    <i class="fa fa-plus" aria-hidden="true"></i>
-                    &nbsp;Add Cleaner
-                </button>
-            </div>
+            <h3>
+                Table(s)
+                <div class="pull-right">
+                    <button class="btn btn-success">
+                        <i class="fa fa-plus" aria-hidden="true"></i>
+                        &nbsp;Add Cleaner
+                    </button>
+                </div>
+            </h3>
+            
             <div class="table-list" style="clear: both;margin-bottom: 10px;"></div>
         </div>
     </div>
