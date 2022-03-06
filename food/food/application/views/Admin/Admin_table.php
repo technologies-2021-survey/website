f774c611fda1030bec2a7bb88212a5588dee3126
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-06 18:01:35 +0800

    Update Admin_table.php

--- food/food/application/views/Admin/Admin_table.php
+++ food/food/application/views/Admin/Admin_table.php
@@ -59,7 +59,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="viewOrderLabel">View Order</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close pull-right" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
