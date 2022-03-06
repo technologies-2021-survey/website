ae3a4446ab11ce0592f834991c053cc96e52a01b
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-06 19:58:07 +0800

    Update Admin_table.php

--- food/food/application/views/Admin/Admin_table.php
+++ food/food/application/views/Admin/Admin_table.php
@@ -307,7 +307,7 @@
             x = x + '<div class="pull-right" style="margin-top: -20px;">';
                 x = x + '<button class="btn btn-primary" style="margin-right: 10px;" onclick="getTableDineInOrder('+data.id+')">View Order</button>';
                 if(data.status == "Waiting") {
-                    x = x + '<button class="btn btn-success" onclick="doneServe('+table_id+','+data.id+')">Done serve</button>';
+                    x = x + '<button class="btn btn-success" onclick="doneServe('+data.table_id+','+data.id+')">Done serve</button>';
                 }
             x = x + '</div>';
             x = x + '<div style="clear: both;"></div>';
