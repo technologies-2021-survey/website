6778f2313573358b94f692ace39adf106c80edc3
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-06 18:24:46 +0800

    Update Admin_menu.php

--- food/food/application/views/Admin/Admin_menu.php
+++ food/food/application/views/Admin/Admin_menu.php
@@ -12,11 +12,91 @@
             </h3>
             <div class="menu-list" style="clear:both;margin-bottom: 10px;">
             </div>
+            <div class="pull-left">
+                <button class="btn btn-primary" id="prev"><i class="fa fa-caret-left" aria-hidden="true"></i>&nbsp;Prev</button>
+            </div>
+            <div class="pull-right">
+                <button class="btn btn-primary" id="next">Next&nbsp;<i class="fa fa-caret-right" aria-hidden="true"></i></button>
+            </div>
+            <div style="clear: both;"></div>
         </div>
     </div>
 </div>
 <script type="text/javascript">
-    $(document).ready(function() {
+    var id = 1;
+    function checkMenu(ids, type = "") {
+        $.ajax({
+            url: "<?php echo base_url(); ?>admin/getMenu/"+ids,
+            type: "GET",
+            success: function(data){
+                data = JSON.parse(data);
+                if(data.length != "") {
+                    notif.play();
+                    if(type == "add") {
+                        id++;
+                        getMenu(id);
+                    } else if(type == "minus") {
+                        id--;
+                        getMenu(id);
+                    }
+                } else {
+                    errorRow();
+                }
+                
+            }
+        });
+    }
+    
+    function getMenu(ids) {
+        $.ajax({
+            url: "<?php echo base_url(); ?>admin/getMenu/"+id,
+            type: "GET",
+            success: function(data){
+                data = JSON.parse(data);
+                $('.table-list').html("");
+                if(data.length != "") {
+                    for(var i = 0; i < data.length; i++) {
+                        addRow(i, data[i]);
+                    }
+                }
+            }
+        });
+    }
+    function errorRow() {
+        notif.play();
+        Toast.fire({
+            icon: 'error',
+            title: 'Error! There\'s no data'
+        });
+    }
 
+    getMenu(id);
+
+    $(document).ready(function() {
+        $('#prev').click(function() {
+            if(id > 1) {
+                var s = id - 1;
+                checkTables(s, 'minus');
+            } else {
+                errorRow();
+            }
+        });
+        $('#next').click(function() {
+            var s = id + 1;
+            checkTables(s, 'add');
+        });
+
+        $('#prev2').click(function() {
+            if(id2 > 1) {
+                var s = id2 - 1;
+                checkDineIn(s2, 'minus');
+            } else {
+                errorRow();
+            }
+        });
+        $('#next2').click(function() {
+            var s = id2 + 1;
+            checkDineIn(s, 'add');
+        });
     });
 </script>
