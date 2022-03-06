000165396854b1fedd4142ecbd5f9db50e8c683c
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-06 16:46:59 +0800

    Update Admin_table.php

--- food/food/application/views/Admin/Admin_table.php
+++ food/food/application/views/Admin/Admin_table.php
@@ -1,2 +1,8 @@
 <div class="col-lg-6 col-md-6">
+    <div class="box-container"> 
+        <div class="box-body">
+            <h3>Table(s)</h3>
+            <div id="calendar"></div>
+        </div>
+    </div>
 </div>
