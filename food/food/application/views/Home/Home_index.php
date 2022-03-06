377d8cb18e44117ee035ab1e18b98ba4ed63e181
Harvey Arboleda <haaaarvs24@gmail.com>
2022-03-06 16:24:06 +0800

    asd

--- food/food/application/views/Home/Home_index.php
+++ food/food/application/views/Home/Home_index.php
@@ -4,7 +4,7 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-lg-12">
-							<img src="https://peekabook.tech/food/food/assets/img/logo.png" style="height: 105px; margin: 0 auto; display: block; margin-top: 90px; margin-bottom: 28px;">
+							<img src="https://peekabook.tech/food/food/assets/img/logo.png" style="height: 12vh; margin: 0 auto; display: block; margin-top: 90px; margin-bottom: 28px;">
 							<p>
 								CAFÉ LIDIA formally opened in September 8, 2009 after its soft opening on August 17, 2009. Operations were cut short because of Typhoon “Ondoy” which came in Sept 26, 2009. We reopened our doors on October 15, 2009 and since then we’ve never looked back. We called the restaurant “LIDIA” after the owner’s name which means Woman of Business. Having been in the shoe manufacturing business for years, the transition was not easy. We experienced the same birthing pains of one who is starting in the business. But with hard work, and perseverance, things got better after three months of operation. We did not spend a penny on any marketing scheme. We became known by word of mouth from our first customers – friends, relatives and school mates. Today, we are proud to have grown into one if not the most well-loved café – restaurant in Marikina. Families, organizations, “barkadas” come to enjoy the
 							</p>
